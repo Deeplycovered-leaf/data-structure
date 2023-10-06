a38e0c79dc634385947ec6aa73f74b9d790a4792
d-leaf <346691836@qq.com>
2023-10-06 18:08:09 +0800

    feat: 链表题目

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,24 @@
 import { expect, it } from 'vitest'
 import { LinkedList } from './LinkedList'
 
-const linked_list = new LinkedList<number>()
-linked_list.append(1)
-linked_list.append(2)
-linked_list.append(3)
+class ListNode {
+  val: number
+  next: ListNode | null
 
-linked_list.insert(1, 0)
-linked_list.insert(0, 4)
-linked_list.traverse()
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val ?? 0
+    this.next = next ?? null
+  }
 
-linked_list.remove(0)
-linked_list.traverse()
+  delete_node(node: ListNode | null): void {
+    if (!node)
+      return
 
-linked_list.remove(2)
-linked_list.traverse()
+    node!.val = node!.next!.val
+    node!.next = node!.next!.next
+  }
+}
 
-console.log('linked_list.get(0) =>', linked_list.get(0))
-console.log('linked_list.get(2) =>', linked_list.get(2))
-
-console.log('linked_list.index_of(0) =>', linked_list.index_of(0))
-
-linked_list.remove_by_value(0)
-linked_list.traverse()
 // if (import.meta.vitest) {
 //   it('should be insert 0 into before 2', () => {
 //     expect(hot_potato(['zi', 'ze', 'zb', 'zd'], 3)).toBe('zi')
